f8d6c75a60a16c87a9558a410a35bb8564f5426b
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 19:31:06 +0800

    write practice 7 class Student
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_7/student.js
+++ src/practice_7/student.js
@@ -1,1 +1,16 @@
+"use strict";
 
+import Person from "./person";
+
+class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        return `${super.introduce()} I am a Student. I am at ${this.klass.getDisplayName()}.`
+    }
+}
+
+export default Student;
