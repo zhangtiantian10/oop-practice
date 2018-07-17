70744685107dd75bd2816338d592f8134f55c4fd
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 21:40:10 +0800

    write practice 11 class Class add registerAssignLeaderListener and registerJoinLister function
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_11/class.js
+++ src/practice_11/class.js
@@ -1,1 +1,43 @@
+"use strict";
 
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    appendMember(student) {
+        student.klass = this;
+        if (this.teacher) {
+            console.log('I am ' + this.teacher.name + '. I know ' + student.name + ' has joined Class ' + this.number + '.');
+        }
+    }
+
+    assignLeader(student) {
+        if (this.isIn(student)) {
+            this.leader = student;
+            if (this.teacher) {
+                console.log('I am ' + this.teacher.name + '. I know ' + student.name + ' become Leader of Class ' + this.number + '.');
+            }
+        } else {
+            console.log("It is not one of us.");
+        }
+    }
+
+    isIn(student) {
+        return student.klass.number === this.number;
+    }
+
+    registerAssignLeaderListener(teacher) {
+        this.teacher = teacher;
+    }
+
+    registerJoinListener(teacher) {
+        this.teacher = teacher;
+    }
+}
+
+export default Class;
