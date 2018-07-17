0adbc0c5b5a95cbb5786e469ae66c9d0e542fffb
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 20:29:45 +0800

    write practice 10 class Class add isIn function
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_10/class.js
+++ src/practice_10/class.js
@@ -1,3 +1,29 @@
-import Class from '../practice_9/class';
+"use strict";
+
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    appendMember(student) {
+        student.klass = this;
+    }
+
+    assignLeader(student) {
+        if (student.klass.number === this.number) {
+            this.leader = student;
+        } else {
+            console.log("It is not one of us.");
+        }
+    }
+
+    isIn(student) {
+        return student.klass.number === this.number;
+    }
+}
 
 export default Class;
