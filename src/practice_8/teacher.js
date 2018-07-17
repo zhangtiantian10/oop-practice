bf1020b78d4b0276192261e62ca0365135f8c238
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 19:57:38 +0800

    write practice 8 class Teacher
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_8/teacher.js
+++ src/practice_8/teacher.js
@@ -1,1 +1,16 @@
+"use strict";
 
+import Person from "./person";
+
+class Teacher extends Person {
+    constructor(id, name, age, klass) {
+        super(id, name, age, klass);
+        this.klass = klass;
+    }
+
+    introduce() {
+        return `${super.introduce()} I am a Teacher. I teach ${this.klass ? this.klass.getDisplayName() : 'No Class'}.`;
+    }
+}
+
+export default Teacher;
