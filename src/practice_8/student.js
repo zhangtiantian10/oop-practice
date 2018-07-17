630120f341a3fc260a10cbc545d84a67d0870115
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 19:57:20 +0800

    write practice 8 class Student, can know student is leader
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_8/student.js
+++ src/practice_8/student.js
@@ -1,1 +1,21 @@
+"use strict";
 
+import Person from "./person";
+
+class Student extends Person{
+    constructor(id, name, age, klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        let str = "at ";
+        const leader = this.klass.leader;
+        if (leader && leader.id === this.id) {
+            str = "Leader of ";
+        }
+        return `${super.introduce()} I am a Student. I am ${str + this.klass.getDisplayName()}.`;
+    }
+}
+
+export default Student;
