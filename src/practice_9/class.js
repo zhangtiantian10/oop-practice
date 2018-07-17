15360c34f1eefb22e0e1cf1ef826e72603ea0915
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 20:12:24 +0800

    write practice 9 class Class add appendMember
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_9/class.js
+++ src/practice_9/class.js
@@ -1,1 +1,25 @@
+"use strict";
 
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    appendMember(student) {
+        student.klass = this;
+    }
+
+    assignLeader(student) {
+        if (student.klass.number === this.number) {
+            this.leader = student;
+        } else {
+            console.log("It is not one of us.");
+        }
+    }
+}
+
+export default Class;
