be938aad0f601f59b2b83b88cb1001b23ec0143a
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 20:25:55 +0800

    write practice 10 class Teacher modify klass to klasses
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,22 @@
+"use strict";
 
+import Person from "./person";
+
+class Teacher extends Person{
+    constructor(id, name, age, klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    introduce() {
+        let str = 'I teach No Class.';
+        if (this.klasses && this.klasses.length) {
+            const classesNumber = this.klasses
+                .map(klass => klass.number).join(', ')
+            str = `I teach Class ${classesNumber}.`
+        }
+        return `${super.introduce()} I am a Teacher. ${str}`
+    }
+}
+
+export default Teacher;
