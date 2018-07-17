3a63fbc5d34e28510ef3c731bc40db0d6b52a7d9
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 19:56:41 +0800

    write practice 8 class Class add leader
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,19 @@
+"use strict";
 
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student) {
+        if (student.klass.number === this.number) {
+            this.leader = student;
+        }
+    }
+}
+
+export default Class;
