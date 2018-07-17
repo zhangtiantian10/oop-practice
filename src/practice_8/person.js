b4b80d63b32c0293a893ce09b70a02cb6a70bbf4
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 19:56:15 +0800

    write practice 8 class Person add id
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_8/person.js
+++ src/practice_8/person.js
@@ -1,1 +1,15 @@
+"use strict";
 
+class Person {
+    constructor(id, name, age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        return `My name is ${this.name}. I am ${this.age} years old.`
+    }
+}
+
+export default Person;
