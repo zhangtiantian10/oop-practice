b916662788c7822791bee931a2a36c7179d0a4d6
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 19:30:50 +0800

    write practice 7 class Class
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/practice_7/class.js
+++ src/practice_7/class.js
@@ -1,1 +1,13 @@
+"use strict";
 
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+}
+
+export default Class;
